9eda373aaf4944ca55e0a819a61a680477b7b77e
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2021-10-30 09:58:20 +0700

    change

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 
-const express = require('express')
-const app = express()
-const port = 3000
-//Routte
+var path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan('combined'));
+//Template engine
+app.engine('html',handlebars({
+    extname: '.html',
+}));
+app.set('view engine','html');
+app.set('views',path.join(__dirname,'resources/views'));
+
+//Route
 app.get('/', (req, res) => {
-    var a=1;
-    var b=2;
-    var c=a+b;
-  res.send('Hello World!')
-})
+ res.render('home');
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
