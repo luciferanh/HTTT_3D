ed91347329931439cb1107cbc80902bb70b6788b
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2021-11-02 14:38:43 +0700

    change

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -2,5 +2,9 @@ class HomeController{
     index(req,res){
         res.render('home');
     }
+    store(req,res){
+            console.log('CCCCCCCCCCCCCCCCC');
+        res.render('home');
+    }
 }
 module.exports =new HomeController;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
-const HomeRouter= require('./home.js');
+const HomeRouter= require('./web.js');
 function route(app){
 
         app.use('/',HomeRouter);
+        app.use('/store',HomeRouter);
+   
 }
 module.exports =route;
