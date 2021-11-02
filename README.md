af5a17f4452006011dfb1a1c4fc60ef9a1e3d3dd
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2021-11-02 21:49:05 +0700

    change

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HTTT_3D
-Web about spatial data 
+Bước 1: Sử dụng npm i để down toàn bộ lib trong node
+Bước 2: Sử dụng npm start để chạy và npm run watch để có thể code CSS ( code file app.scss tự reload sang css)
+Bước 3: đặt tk sa với mk 123456 của SQL Server 
--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,9 +1,11 @@
+const sql = require('../config/database.js');
 class HomeController{
     index(req,res){
         res.render('home');
     }
     store(req,res){
             console.log(req.body);
+            sql.getdata();
         res.render('home');
     }
 }
