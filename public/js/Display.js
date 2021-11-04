160d6bb568dacb40996d0a01ac068c0cd61a59a3
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2021-11-04 10:48:18 +0700

    change

--- public/js/Display.js
+++ public/js/Display.js
@@ -1,7 +1,12 @@
 const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xffffffff );
 const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
+const axesHelper = new THREE.AxesHelper(20,20 );
+axesHelper.setColors(0x00f00);
+const axesHelper2 = new THREE.AxesHelper(-100 );
+axesHelper2.setColors(0x00f00);
+scene.add( axesHelper );
+scene.add( axesHelper2 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth/1.2, window.innerHeight/1.5 );
 document.getElementById("GiaoDien").appendChild( renderer.domElement );
