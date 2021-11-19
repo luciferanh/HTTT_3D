5ba210cd9dbe58a3636c105eaf8be4f4795627df
Hoàng An <hoanganmsc@gmail.com>
2021-11-19 18:19:27 +0700

    Add files via upload

--- js/Slidebar.js
+++ js/Slidebar.js
@@ -31,6 +31,20 @@ list.addEventListener('click', function (ev) {
         ev.target.classList.toggle('checked');
     }
 }, false);
+var inputSrc = document.getElementById("myInput").value;
+switch(inputSrc)
+{
+    case "KTX Khu A":
+        {
+            inputSrc.innerHTML =
+            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3918.1227235628844!2d106.80443711402478!3d10.878269360282045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1zS1RYIEtodSBBIMSQSFFHIFRQLkhDTSwga2h1IHBo4buRIDYsIMSQw7RuZyBIw7JhLCBUaOG7pyDEkOG7qWMsIEhvIENoaSBNaW5oIENpdHk!5e0!3m2!1sen!2s!4v1637320280611!5m2!1sen!2s" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>;
+        }
+    case "KTX Khu B":
+        {
+            inputSrc.innerHTML =
+            <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3918.1227235628844!2d106.80443711402478!3d10.878269360282045!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1zS1RYIEtodSBBIMSQSFFHIFRQLkhDTSwga2h1IHBo4buRIDYsIMSQw7RuZyBIw7JhLCBUaOG7pyDEkOG7qWMsIEhvIENoaSBNaW5oIENpdHk!5e0!3m2!1sen!2s!4v1637320280611!5m2!1sen!2s" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>;
+        }
+}
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
