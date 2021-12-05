1c17f591e6f2bd31846e365f69f41de055b9c37a
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2021-12-05 20:12:39 +0700

    Revert "change sql"
    This reverts commit c52e2c5ebfbbccbd77242f9018baf555e5618fda.

--- config/SQLQuery1.sql
+++ config/SQLQuery1.sql
@@ -1,4 +1,4 @@
-﻿create database dialy
+create database dialy
 use dialy
 create table Nguoi_dung (
 	ID int NOT NULL PRIMARY KEY,
@@ -6,25 +6,22 @@ create table Nguoi_dung (
     FirstName varchar(255),
 	MatKhau varchar(255),
 	SDT varchar(10),
-	x_diachi float,
-	y_diachi float,
+	x float,
+	y float,
 	tinhtrang varchar(255),
 	DiaChi varchar(255),
     City varchar(255),
 	Toa_do geography ,
-
 )
-
 create table HoDuongTinh (
 	ID int NOT NULL PRIMARY KEY,
 	LastName varchar(255),
     FirstName varchar(255),
-	NamSinh int,
 	SDT varchar(10),
 	x float,
 	y float,
 	DiaChi varchar(255),
-	NgayBiDuongTinh datetime,
+	NgayBiDuongTinh smalldatetime,
 	tinhtrang varchar(255),
     City varchar(255),
 	Toa_do geography ,
@@ -36,12 +33,6 @@ Create table User_Ho(
 	CONSTRAINT FK_ND FOREIGN KEY (NguoiDungID) REFERENCES Nguoi_dung(ID),
 	CONSTRAINT FK_Ho FOREIGN KEY (HoID) REFERENCES HoDuongTinh(ID)
 )
--- Thêm Nguoi Dung
+-- Th�m Nguoi Dung
 
-INSERT INTO Nguoi_dung (ID,FirstName,LastName ,MatKhau,SDT,x_diachi,y_diachi,tinhtrang,DiaChi,City,Toa_do) VALUES (0,'Tran','Anh','123456','0832012355',10.869596,106.803244,'Âm tính','UIT','TpHCM',geography::Point(10.869596, 106.803244, 4326))
-
---Thêm ca bệnh
-SET DATEFORMAT DMY  
-INSERT INTO HoDuongTinh (ID,FirstName,LastName ,NamSinh,SDT,x,y,tinhtrang,DiaChi,NgayBiDuongTinh,City,Toa_do) VALUES (0,'Nguyen','Teo',1999,'0832012355',10.869948,106.796439,'Dương tính','UIT','18/05/2020','TpHCM',geography::Point(10.869948,106.796439, 4326))
-INSERT INTO HoDuongTinh (ID,FirstName,LastName ,NamSinh,SDT,x,y,tinhtrang,DiaChi,NgayBiDuongTinh,City,Toa_do) VALUES (1,'Nguyen','Ti',1999,'0832012355',10.874842, 106.798475,'Dương tính','UIT','20/05/2020','TpHCM',geography::Point(10.874842, 106.798475, 4326))
-
+INSERT INTO 
