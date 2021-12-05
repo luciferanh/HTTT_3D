c7d6474d588f79a4578e5d146a0c8731fba22d0d
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2021-12-05 13:56:13 +0700

    change

--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -1,36 +1,105 @@
 
 var mapObj = null;
+//--------------------------------Layer Map---------------------------------------------
+var grayscale =L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}),
+    streets   = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'});
+    
+
 var defaultCoord = [10.869596, 106.803244]; // coord mặc định, UIT
 var zoomLevel = 16; // Mức phóng to bản đồ
+
+
+var baseMaps = {
+    "Grayscale": grayscale,
+    "Streets": streets
+};
+//---------------------------------------------------------------------------------------------------------------------
+
+//--------------------------------Marker ---------------------------------------------
+var fg = L.featureGroup();
+var array_duongtinh= [[10.869948, 106.796439],[10.874842, 106.798475]];
+var layers= L.layerGroup()
+
+for (let index = 0; index < array_duongtinh.length; index++) {
+    const element = array_duongtinh[index];
+    var Duongtinh = element; // Toạ độ marker
+    var option = {
+        className: "map-popup-content",
+    };
+// html cho popup
+    var content = `<div class='left'>
+                            <img src='https://upload.wikimedia.org/wikipedia/vi/2/28/Bloom.png' />
+                        </div>
+                        <div class='right'>
+                            <b>Ca thứ xx</b><br>Dương tính `+index+`
+                        </div>
+                        <div class='clearfix'></div>`;
+
+    var popup = L.popup(option);
+    popup.setContent(content);
+    var Icon = new L.Icon({
+        iconUrl: 'img/icon/duongtinh.png',
+        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+        popupAnchor: [1, -34],
+        shadowSize: [41, 41]
+    });
+    var marker = L.marker(Duongtinh,{
+        icon: Icon}
+    );
+    marker.bindPopup(popup);
+    marker.addTo(fg);
+    layers.addLayer(fg);
+    
+    // binding popup vào marker
+    
+    
+}
+//-----------------------------------------------------------------------------------------------------------
+
 var mapConfig = {
     attributionControl: false, // để ko hiện watermark nữa, nếu bị liên hệ đòi thì nhớ open nha
     center: defaultCoord, // vị trí map mặc định hiện tại
     zoom: zoomLevel,
+    layers: [grayscale, layers]
 };
 
 mapObj = L.map('map', mapConfig);
+var overlayMaps = {
+    "Ca dương tính": layers
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(mapObj);
+var baseMaps = {
+    "<span style='color: gray'>Grayscale</span>": grayscale,
+    "Streets": layers
+};
 
 // thêm tile để map có thể hoạt động, xài free từ OSM
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(mapObj);
-
-// Add custom marker
-var marker_coord = [10.869596, 106.803244]; // Toạ độ marker
-var popup_option = {
-    className: "map-popup-content",
-};
-// html cho popup
-var popup_content = `<div class='left'>
-                        <img src='https://img.pixers.pics/pho(s3:700/PI/23/27/700_PI2327_65c65c262917a837fe5b7240420e1ab4_5b7ab916358ae_.,700,700,jpg)/posters-hello-kitty.jpg.jpg' />
-                    </div>
-                    <div class='right'>
-                        <b>Sudo - OpenStreetMap</b><br>Việt Anh đẹp trai
-                    </div>
-                    <div class='clearfix'></div>`;
 
 
-var marker = L.marker(marker_coord).addTo(mapObj);
+// // Add custom marker
+// var marker_coord = [10.869596, 106.803244]; // Toạ độ marker
+// var popup_option = {
+//     className: "map-popup-content",
+// };
+// // html cho popup
+// var popup_content = `<div class='left'>
+//                         <img src='https://img.pixers.pics/pho(s3:700/PI/23/27/700_PI2327_65c65c262917a837fe5b7240420e1ab4_5b7ab916358ae_.,700,700,jpg)/posters-hello-kitty.jpg.jpg' />
+//                     </div>
+//                     <div class='right'>
+//                         <b>Sudo - OpenStreetMap</b><br>Việt Anh đẹp trai
+//                     </div>
+//                     <div class='clearfix'></div>`;
+
+// var popup = L.popup(popup_option);
+// popup.setContent(popup_content);
+
+// var marker = L.marker(marker_coord).addTo(mapObj);
+// // binding popup vào marker
+// marker.bindPopup(popup);
+
 
 
 
--- public/js/slidebar_checkbox.js
+++ public/js/slidebar_checkbox.js
@@ -1,46 +1,28 @@
+var fg = L.featureGroup();
 
 function CheckDuongtinh(){
 // Test ca dương tính 
-    var Duongtinh1 = [10.869948, 106.796439]; // Toạ độ marker
-    var Duongtinh1_option = {
-        className: "map-popup-content",
-    };
-    // html cho popup
-        var Duongtinh1_content = `<div class='left'>
-                                <img src='https://upload.wikimedia.org/wikipedia/vi/2/28/Bloom.png' />
-                            </div>
-                            <div class='right'>
-                                <b>Ca thứ xx</b><br>Dương tính 1
-                            </div>
-                            <div class='clearfix'></div>`;
-
-        var popup1 = L.popup(Duongtinh1_option);
-        popup1.setContent(Duongtinh1_content);
-        var greenIcon = new L.Icon({
-            iconUrl: 'img/icon/duongtinh.png',
-            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-            iconSize: [25, 41],
-            iconAnchor: [12, 41],
-            popupAnchor: [1, -34],
-            shadowSize: [41, 41]
-        });
-
-        var Duongtinh2 = [10.874842, 106.798475]; // Toạ độ marker
-        var Duongtinh2_option = {
-            className: "map-popup-content",
-        };
+    var array_duongtinh= [[10.869948, 106.796439],[10.874842, 106.798475]];
+  
+    if ($('#toggle').is(":checked")) {
+        for (let index = 0; index < array_duongtinh.length; index++) {
+            const element = array_duongtinh[index];
+            var Duongtinh = element; // Toạ độ marker
+            var option = {
+                className: "map-popup-content",
+            };
         // html cho popup
-            var Duongtinh2_content = `<div class='left'>
+            var content = `<div class='left'>
                                     <img src='https://upload.wikimedia.org/wikipedia/vi/2/28/Bloom.png' />
                                 </div>
                                 <div class='right'>
-                                    <b>Ca thứ xx</b><br>Dương tính 2
+                                    <b>Ca thứ xx</b><br>Dương tính `+index+`
                                 </div>
                                 <div class='clearfix'></div>`;
-        
-            var popup2 = L.popup(Duongtinh2_option);
-            popup2.setContent(Duongtinh2_content);
-            var greenIcon = new L.Icon({
+    
+            var popup = L.popup(option);
+            popup.setContent(content);
+            var Icon = new L.Icon({
                 iconUrl: 'img/icon/duongtinh.png',
                 shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
                 iconSize: [25, 41],
@@ -48,28 +30,20 @@ function CheckDuongtinh(){
                 popupAnchor: [1, -34],
                 shadowSize: [41, 41]
             });
-            var marker1 = L.marker(Duongtinh1,{
-                icon: greenIcon}
+            var marker = L.marker(Duongtinh,{
+                icon: Icon}
             );
-            var marker2 = L.marker(Duongtinh2,{
-                icon: greenIcon}
-            )
-    if ($('#toggle').is(":checked")) {
-        console.log("123");
-        marker1.addTo(mapObj);
-        // binding popup vào marker
-        marker1.bindPopup(popup1);
-        marker2.addTo(mapObj);
-        // binding popup vào marker
-        marker2.bindPopup(popup2);
+            marker.bindPopup(popup);
+            marker.addTo(fg);
+            var layer = L.layerGroup().addLayer(fg);
+          
+            // binding popup vào marker
+            
+            
+        }
+    
     } else {
-        mapObj.eachLayer(function(layer) {
-            if (layer instanceof L.MarkerClusterGroup)
-            {
-                map.removeLayer(layer)
-            }
-        })
-        
+        map.removeLayer(layer)
         
     }
 }
