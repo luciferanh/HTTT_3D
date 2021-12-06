c29059c8049f8090dd9fa96c7ea149bad59c062d
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2021-12-06 11:15:34 +0700

    change sql

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,15 +1,48 @@
-const sql = require('../config/database.js');
+const sql = require("mssql/msnodesqlv8");
+const config = {
+    server:"localhost",
+    user: "sa",
+    password: "123456",
+    server: "DESKTOP-OT7KKUK",
+    database: "dialy",
+    driver:"msnodesqlv8",
+    options: {
+      trustedconnection: true,
+      enableArithAbort: true,
+      instancename: "",
+    },
+ 
+};
+
 class HomeController {
     index(req, res) {
+        console.log("dang index");
         res.render('home');
     }
     store(req, res) {
-        console.log(req.body);
-        sql.getdata();
-        res.render('home');
+        console.log("dang store");
+        sql.connect(config, function (err) {
+    
+            if (err) console.log(err);
+    
+            // create Request object
+            var request = new sql.Request();
+               
+            // query to the database and get the records
+            request.query('select * from HoDuongTinh', function (err, recordset) {
+                
+                if (err) console.log(err)
+    
+                // send records as a response
+                console.log(recordset);
+                res.json(recordset);
+              
+                
+            });
+        });
+       
     }
-    login(req, res) {
-        res.render('user/auth');
-    }
+
+
 }
 module.exports = new HomeController;
--- public/js/leaflet.js
+++ public/js/leaflet.js
@@ -1,98 +1,112 @@
 
-var mapObj = null;
-//--------------------------------Layer Map---------------------------------------------
-var     streets   = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { id: 'MapID', attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'});
+$.get("/store", function(data) {
+    DatabaseDT= Object.values(data)[1];
+
+    var mapObj = null;
+    //--------------------------------Layer Map---------------------------------------------
+    var     streets   = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', { id: 'MapID', attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'});
+        
+
+    var defaultCoord = [10.869596, 106.803244]; // coord mặc định, UIT
+    var zoomLevel = 16; // Mức phóng to bản đồ
+
+    //---------------------------------------------------------------------------------------------------------------------
+
+    //--------------------------------Marker ---------------------------------------------
+    var fg = L.featureGroup();
+    var array_duongtinh= [[10.869948, 106.796439],[10.874842, 106.798475]];
+    var layers_array= [];
+
+    for (let index = 0; index < DatabaseDT.length; index++) {
+          const element = [DatabaseDT[index].x,DatabaseDT[index].y];
+        var Duongtinh = element; // Toạ độ marker
+        var option = {
+            className: "map-popup-content",
+        };
+    // html cho popup
+        var content = `<div class='left'>
+                                <img src='https://upload.wikimedia.org/wikipedia/vi/2/28/Bloom.png' />
+                            </div>
+                            <div class='right'>
+                                <b>Ca thứ xx</b><br>Dương tính `+index+`
+                            </div>
+                            <div class='clearfix'></div>`;
+
+        var popup = L.popup(option);
+        popup.setContent(content);
+        var Icon = new L.Icon({
+            iconUrl: 'img/icon/duongtinh.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+        var marker = L.marker(Duongtinh,{
+            icon: Icon}
+        );
+        marker.bindPopup(popup);
     
-
-var defaultCoord = [10.869596, 106.803244]; // coord mặc định, UIT
-var zoomLevel = 16; // Mức phóng to bản đồ
-
-//---------------------------------------------------------------------------------------------------------------------
-
-//--------------------------------Marker ---------------------------------------------
-var fg = L.featureGroup();
-var array_duongtinh= [[10.869948, 106.796439],[10.874842, 106.798475]];
-var layers_array= [];
-
-for (let index = 0; index < array_duongtinh.length; index++) {
-    const element = array_duongtinh[index];
-    var Duongtinh = element; // Toạ độ marker
-    var option = {
-        className: "map-popup-content",
+        layers_array.push(marker);
+        
+        // binding popup vào marker
+        
+        
+    }
+    //-----------------------------------------------------------------------------------------------------------
+    var duongtinh = L.layerGroup(layers_array);
+    var mapConfig = {
+        attributionControl: true, // để ko hiện watermark nữa, nếu bị liên hệ đòi thì nhớ open nha
+        center: defaultCoord, // vị trí map mặc định hiện tại
+        zoom: zoomLevel,
+        layers: [streets, duongtinh]
     };
-// html cho popup
-    var content = `<div class='left'>
-                            <img src='https://upload.wikimedia.org/wikipedia/vi/2/28/Bloom.png' />
-                        </div>
-                        <div class='right'>
-                            <b>Ca thứ xx</b><br>Dương tính `+index+`
-                        </div>
-                        <div class='clearfix'></div>`;
 
-    var popup = L.popup(option);
-    popup.setContent(content);
-    var Icon = new L.Icon({
-        iconUrl: 'img/icon/duongtinh.png',
-        shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-        iconSize: [25, 41],
-        iconAnchor: [12, 41],
-        popupAnchor: [1, -34],
-        shadowSize: [41, 41]
-    });
-    var marker = L.marker(Duongtinh,{
-        icon: Icon}
-    );
-    marker.bindPopup(popup);
- 
-    layers_array.push(marker);
+    mapObj = L.map('map', mapConfig);
+
+    // var baseMaps = {
+    //     "Streets": streets
+    // };
+
+    var overlayMaps = {
     
-    // binding popup vào marker
-    
-    
-}
-//-----------------------------------------------------------------------------------------------------------
-var duongtinh = L.layerGroup(layers_array);
-var mapConfig = {
-    attributionControl: true, // để ko hiện watermark nữa, nếu bị liên hệ đòi thì nhớ open nha
-    center: defaultCoord, // vị trí map mặc định hiện tại
-    zoom: zoomLevel,
-    layers: [streets, duongtinh]
-};
+        "<img src='img/icon/duongtinh.png' /><span class='my-layer-item'>Ca F0</span>":duongtinh
 
-mapObj = L.map('map', mapConfig);
+    }
 
-// var baseMaps = {
-//     "Streets": streets
-// };
+    L.control.layers( [], overlayMaps,).addTo(mapObj);
 
-var overlayMaps = {
-  
-    "<img src='img/icon/duongtinh.png' /><span class='my-layer-item'>Ca F0</span>":duongtinh
+    // // Add custom marker
+    // var marker_coord = [10.869596, 106.803244]; // Toạ độ marker
+    // var popup_option = {
+    //     className: "map-popup-content",
+    // };
+    // // html cho popup
+    // var popup_content = `<div class='left'>
+    //                         <img src='https://img.pixers.pics/pho(s3:700/PI/23/27/700_PI2327_65c65c262917a837fe5b7240420e1ab4_5b7ab916358ae_.,700,700,jpg)/posters-hello-kitty.jpg.jpg' />
+    //                     </div>
+    //                     <div class='right'>
+    //                         <b>Sudo - OpenStreetMap</b><br>Việt Anh đẹp trai
+    //                     </div>
+    //                     <div class='clearfix'></div>`;
 
-}
+    // var popup = L.popup(popup_option);
+    // popup.setContent(popup_content);
 
-L.control.layers( [], overlayMaps,).addTo(mapObj);
+    // var marker = L.marker(marker_coord).addTo(mapObj);
+    // // binding popup vào marker
+    // marker.bindPopup(popup);
+    function onMapClick(e) {
+        alert("You clicked the map at " + e.latlng);
+      }
+      
+      mapObj.on('click', onMapClick);
 
-// // Add custom marker
-// var marker_coord = [10.869596, 106.803244]; // Toạ độ marker
-// var popup_option = {
-//     className: "map-popup-content",
-// };
-// // html cho popup
-// var popup_content = `<div class='left'>
-//                         <img src='https://img.pixers.pics/pho(s3:700/PI/23/27/700_PI2327_65c65c262917a837fe5b7240420e1ab4_5b7ab916358ae_.,700,700,jpg)/posters-hello-kitty.jpg.jpg' />
-//                     </div>
-//                     <div class='right'>
-//                         <b>Sudo - OpenStreetMap</b><br>Việt Anh đẹp trai
-//                     </div>
-//                     <div class='clearfix'></div>`;
 
-// var popup = L.popup(popup_option);
-// popup.setContent(popup_content);
 
-// var marker = L.marker(marker_coord).addTo(mapObj);
-// // binding popup vào marker
-// marker.bindPopup(popup);
+
+
+});
 
 
 
@@ -124,13 +138,4 @@ L.control.layers( [], overlayMaps,).addTo(mapObj);
 
 
 
-
-
-
-
- function onMapClick(e) {
-  alert("You clicked the map at " + e.latlng);
-}
-
-mapObj.on('click', onMapClick);
 
