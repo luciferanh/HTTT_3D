d4a3d310b7b6376fa326f60f026a58d83fbad032
Trần Việt Anh <62204282+luciferanh@users.noreply.github.com>
2021-12-07 20:20:45 +0700

    change

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 const sql = require("mssql/msnodesqlv8");
-const config = {
+const configAnh = {
     server:"localhost",
     user: "sa",
     password: "123456",
-    //server: "DESKTOP-OT7KKUK" ,
-    server: "DESKTOP-I7P0VBM\\SQLEXPRESS" ,// An
+    server: "DESKTOP-OT7KKUK",
+    // server: "DESKTOP-I7P0VBM\\SQLEXPRESS",//An
     database: "dialy",
     driver:"msnodesqlv8",
     options: {
@@ -14,26 +14,45 @@ const config = {
     },
  
 };
+const configAn = {
+  server:"localhost",
+  user: "sa",
+  password: "123456",
 
- function getdataHoDuongTinh(){
-    sql.connect(config, function (err) {
+  server: "DESKTOP-I7P0VBM\\SQLEXPRESS",//An
+  database: "dialy",
+  driver:"msnodesqlv8",
+  options: {
+    trustedconnection: true,
+    enableArithAbort: true,
+    instancename: "",
+  },
+
+};
+connect_may= [configAnh,configAn]
+ function getdataHoDuongTinh(res){
+   for (let index = 0; index <connect_may.length;index++){
+        sql.connect(connect_may[index], function (err) {
+        
+          if (err) console.log(err);
+
+          // create Request object
+          var request = new sql.Request();
+            
+          // query to the database and get the records
+          request.query('select * from HoDuongTinh', function (err, recordset) {
+              
+              if (err) console.log(err)
+
+              // send records as a response
+              console.log(recordset);
+              res.json(recordset);
+            
+              
+          });
+      });
+   }
     
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select * from HoDuongTinh', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            
-            return recordset;
-            
-        });
-    });
 }
   module.exports = {
     getdataHoDuongTinh: getdataHoDuongTinh,
--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -1,20 +1,4 @@
-const sql = require("mssql/msnodesqlv8");
-const config = {
-    server:"localhost",
-    user: "sa",
-    password: "123456",
-    //server: "DESKTOP-OT7KKUK",
-    server: "DESKTOP-I7P0VBM\\SQLEXPRESS",//An
-    database: "dialy",
-    driver:"msnodesqlv8",
-    options: {
-      trustedconnection: true,
-      enableArithAbort: true,
-      instancename: "",
-    },
- 
-};
-
+const database = require('../config/database.js');
 class HomeController {
     index(req, res) {
         console.log("dang index");
@@ -22,25 +6,8 @@ class HomeController {
     }
     store(req, res) {
         console.log("dang store");
-        sql.connect(config, function (err) {
-    
-            if (err) console.log(err);
-    
-            // create Request object
-            var request = new sql.Request();
-               
-            // query to the database and get the records
-            request.query('select * from HoDuongTinh', function (err, recordset) {
-                
-                if (err) console.log(err)
-    
-                // send records as a response
-                console.log(recordset);
-                res.json(recordset);
-              
-                
-            });
-        });
+        database.getdataHoDuongTinh(res);
+
        
     }
     phongtoa(req, res){
